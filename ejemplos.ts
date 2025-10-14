06c7f460d2df4e74ca1fd911845a7bd228742ef3
Victor Parras <vipoo.info@gmail.com>
2025-10-14 13:46:39 +0200

    Some JS exercises

--- ejemplos.ts
+++ ejemplos.ts
@@ -1,3 +1,4 @@
+//Funcion simple en la que sumamos dos numeros que le pasamos como parametros
 function sum(a: number, b: number): number {
   return a + b;
 }
@@ -5,21 +6,74 @@ function sum(a: number, b: number): number {
 console.log(sum(2, 3));
 console.log(sum(5, 7));
 
+//Funcion que multiplica dos numeros y devuelve el resultado
 function multiply(a: number, b: number): number {
-
   return a * b;
 }
 
 let result: number = multiply(3, 4);
 console.log(`El resultado es ${result}`);
 
-
-
-
-function devolverUnSaludo(name:string):string{
-
-let saludo:string = "Hola " + name + ", ¿cómo estás?";
-return saludo;
-} 
+//Funcion que devuelve un saludo personalizado
+function devolverUnSaludo(name: string): string {
+  let saludo: string = "Hola " + name + ", ¿cómo estás?";
+  return saludo;
+}
 
 console.log(devolverUnSaludo("Juan"));
+
+//=================================================================================
+
+//Funciones como parametros, dependiendo de la operacion que queramos hacer
+//le pasamos una funcion u otra
+
+function operarNumeros(operation: (x: number, y: number) => number) {
+  return (a: number, b: number) => operation(a, b);
+}
+
+function sumarNumeros(a: number, b: number): number {
+  return a + b;
+}
+
+function multiplicarNumeros(a: number, b: number): number {
+  return a * b;
+}
+
+console.log(operarNumeros(sumarNumeros)(4, 5)); // 9
+console.log(operarNumeros(multiplicarNumeros)(4, 5)); // 20
+
+
+const numeros: number[] = [1, 2, 3, 4, 5];
+
+numeros.forEach((num) => {
+  let resultado = num * 2;
+  console.log(resultado);
+});
+
+
+let frutas: string[] = ["manzana", "pera", "uva"];
+
+let empty = new Array(0);
+
+console.log(frutas[1]); // pera
+
+frutas.push("melocotón");
+console.log(frutas)
+
+frutas.pop();
+console.log(frutas)
+
+frutas.unshift("sandía");
+console.log(frutas)
+
+frutas.shift();
+console.log(frutas)
+
+let n1: number = Math.floor(Math.random()* frutas.length);
+console.log(n1+" Es la posicion aleatoria");
+console.log(frutas[n1]+" Es la fruta aleatoria");
+
+
+frutas.forEach((fruta,i) => {
+    console.log(`La fruta en la posicion ${i} es ${fruta}`);
+})
---
+++ js-exercises.js
@@ -0,0 +1,130 @@
+//1
+
+let curso = "JS Básico";
+
+function mostrar() {
+  let tema = "Funciones";
+  console.log(curso);
+  console.log(tema);
+}
+mostrar();
+
+//2
+console.log(
+  "===================================================================================="
+);
+
+function saludar(name) {
+  return "Hola " + name;
+}
+
+let saludo = saludar("Vicky");
+console.log(saludo);
+
+//3
+console.log(
+  "===================================================================================="
+);
+
+const miLogger = function (saludo) {
+  return saludo;
+};
+console.log(miLogger("probando"));
+
+//4
+console.log(
+  "===================================================================================="
+);
+const esParn = (n) => n % 2 == 0;
+
+console.log(esParn(3));
+
+//5
+console.log(
+  "===================================================================================="
+);
+
+let fruits = ["aguacate", "uva", "albaricoque"];
+
+console.log(fruits[0]);
+
+fruitlong = fruits.length - 1;
+
+console.log(fruits[fruitlong]);
+
+//6
+console.log(
+  "===================================================================================="
+);
+
+let cola = [];
+cola.push("Ana", "Pepe");
+cola.unshift("Luisa");
+let attp = cola.shift();
+
+console.log(attp);
+console.log(cola);
+
+//7
+console.log(
+  "===================================================================================="
+);
+
+const items = ["altura", "base", "cuadrado", "bisectriz"];
+
+items.forEach((elem, i) => {
+  console.log(elem);
+  console.log(i);
+});
+
+const ITEM = items.indexOf("base");
+
+console.log(ITEM);
+
+//8
+
+console.log(
+  "===================================================================================="
+);
+
+let letras = ["a", "b", "c", "d"];
+
+let fletra = letras.slice(0, 1);
+
+let lletra = letras.slice(letras.length - 3);
+
+console.log(fletra);
+
+console.log(lletra);
+
+//9
+console.log(
+  "===================================================================================="
+);
+
+let n1 = [1,2]
+let n2 = [3,4]
+
+let n3con =n1.concat(n2);
+
+console.log(n1);
+console.log(n2);
+console.log(n3con);
+
+//10
+console.log(
+  "===================================================================================="
+);
+
+const N10 = [2,5,8,12]
+
+let n10some = N10.some(num => num >10)
+let n10every = N10.every(num => num >0)
+let n10include = N10.includes(num => num ===0)
+
+console.log(n10some)
+console.log(n10every);
+console.log(n10include);
+
+//11
+
